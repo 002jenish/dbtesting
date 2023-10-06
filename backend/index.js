2c1e9d47a7a256d9f7a8a147051f9a0c6d597060
002jenish <jenishspacefire314@gmial.com>
2023-10-06 10:38:46 +0530

    backend upload

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+/*
+1. initialize express server(required cors and body0parser)
+2. initialize mongoose
+3. create connection with database
+4. create schema to store the data 
+5. perform crud operation
+
+echo "# dbUserTesting" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/002jenish/dbUserTesting.git
+git push -u origin main
+*/
+
+const express = require('express');  //to start express server
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://jenish1234:jenish_1234@todo.8jbdgxm.mongodb.net/');
+  console.log('db connected')
+}
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+
+// CRUD - Create
+server.post('/demo',async (req,res)=>{
+    //create query
+    let user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    const doc = await user.save();
+
+    console.log(doc);
+    res.json(doc);
+})
+
+//read query
+server.get('/demo',async (req,res)=>{
+    const docs = await User.find({});
+    res.json(docs)
+})
+
+
+server.listen(8080,()=>{
+    console.log('server started')
+})
--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+/*import { useState } from 'react'
+function App() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const handleOnSubmit = async (e) => {
+		e.preventDefault();
+		let result = await fetch(
+		'http://localhost:5000/register', {
+			method: "post",
+			body: JSON.stringify({ name, email }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		result = await result.json();
+		console.warn(result);
+		if (result) {
+			alert("Data saved succesfully");
+			setEmail("");
+			setName("");
+		}
+	}
+	return (
+		<>
+			<h1>This is React WebApp </h1>
+			<form action="">
+				<input type="text" placeholder="name"
+				value={name} onChange={(e) => setName(e.target.value)} />
+				<input type="email" placeholder="email"
+				value={email} onChange={(e) => setEmail(e.target.value)} />
+				<button type="submit"
+				onClick={handleOnSubmit}>submit</button>
+			</form>
+
+		</>
+	);
+}
+
+export default App;
+*/
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [form, setForm] = useState({});
+  const [users, setUsers] = useState([]);
+
+  const handleForm = (e)=>{
+    setForm({
+      ...form,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/demo',{
+      method:'POST',
+      body:JSON.stringify(form),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    const data = await response.json();
+   console.log(data);
+  }
+
+  const getUsers = async ()=>{
+    const response = await fetch('http://localhost:8080/demo',{
+      method:'GET',
+    })
+   const data = await response.json();
+   setUsers(data);
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <span>username</span>
+        <input type="text" name="username" onChange={handleForm}></input>
+        <span>password</span>
+        <input type="text" name="password" onChange={handleForm}></input>
+        <input type="submit"></input>
+      </form>
+      <div>
+        <ul>
+          {users.map(user=><li key={user._id}>{user.username}</li>)}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
